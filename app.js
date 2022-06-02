e06518fffdbc8a2542da039648fd2039d078c4a5
Tashasun <natalia.limbakh@gmail.com>
2022-06-02 23:33:59 +0300

    refactor: add 'alt' according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.classList.add("button")
 
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt = 'delete'
     deleteButton.appendChild(deleteButtonImg);
 
 
